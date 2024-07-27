c2d243a306d4b62915a5f752283e8969e619e95b
Mohith Aiyappa <mohithaiyappa229@gmail.com>
2024-07-27 15:29:36 +0530

    :art: enable edge-to-edge in MainActivity

--- app/src/main/java/tk/mohithaiyappa/spacexlaunchtracker/MainActivity.kt
+++ app/src/main/java/tk/mohithaiyappa/spacexlaunchtracker/MainActivity.kt
@@ -1,6 +1,8 @@
 package tk.mohithaiyappa.spacexlaunchtracker
 
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,7 +11,10 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
